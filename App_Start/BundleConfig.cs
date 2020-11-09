9b749bb36accde2df163f45381d29676d5b584d7
Ignacio Aguero <ignaciomaguero@gmail.com>
2020-11-09 17:24:04 -0300

    se agrega angular

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,7 +8,16 @@ namespace VirtualWallet
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/Script/Bundles").Include(
+               "~/bundles/inline.*",
+               "~/bundles/polyfills.*",
+               "~/bundles/scripts.*",
+               "~/bundles/vendor.*",
+               "~/bundles/runtime.*",
+               "~/bundles/zone.*",
+               "~/bundles/main.*"));
+            bundles.Add(new StyleBundle("~/Content/Styles").Include("~/bundles/styles.*"));
+            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -24,7 +33,7 @@ namespace VirtualWallet
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css"));*/
         }
     }
 }

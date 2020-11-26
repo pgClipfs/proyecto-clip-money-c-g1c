68e98e0cb0810fdc951e22887c9640e7b4ffa9cd
Ignacio Aguero <ignaciomaguero@gmail.com>
2020-11-26 17:18:26 -0300

    interceptor and complements creation, backend setup

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ListaCreditosComponent } from './creditos/lista-creditos/lista-creditos.component';
+import { CuentasCreditoComponent } from './creditos/cuentas-credito/cuentas-credito.component';
 
+import { AuthInterceptorService } from './interceptors/auth-interceptor.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component'
+import { LoginComponent } from './components/login/login.component';
+import { CreditosComponent } from './creditos/creditos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent, 
+    LoginComponent, 
+    CreditosComponent, 
+    FooterComponent, 
+    ListaCreditosComponent,
+    CuentasCreditoComponent
   ],
+
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

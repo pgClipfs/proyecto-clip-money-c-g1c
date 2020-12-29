76a01a302717eb28d62e4046b6cce4db0d0601f5
Ignacio Aguero <ignaciomaguero@gmail.com>
2020-12-29 18:56:36 -0300

    backend configuration

---
+++ Controllers/OperacionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VirtualWallet.Models;
+
+namespace VirtualWallet.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/operacion")]
+    public class OperacionController : ApiController
+    {
+        //[Authorize]
+        // GET: api/Operacion
+        public IEnumerable<Operacion> Get()
+        {
+            GestorOperacion gOperacion = new GestorOperacion();
+            return gOperacion.ObtenerOperaciones();
+        }
+    }
+}
---
+++ Models/GestorCuenta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace VirtualWallet.Models
+{
+    public class GestorCuenta
+    {
+        private string StrConn;
+        public GestorCuenta()
+        {
+            this.StrConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+        }
+            public int AgregarCuenta(Cuenta nueva)
+        {
+
+            int id = 0;
+
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+
+                SqlCommand comm = conn.CreateCommand();
+                comm.CommandText = "agregar_cuenta";
+                comm.CommandType = System.Data.CommandType.StoredProcedure;
+                comm.Parameters.Add(new SqlParameter("@Moneda", nueva.Moneda));
+                //comm.Parameters.Add(new SqlParameter("@Banco", nueva.Banco));
+                //comm.Parameters.Add(new SqlParameter("@NumeroCuenta", nueva.NumeroCuenta));
+                comm.Parameters.Add(new SqlParameter("@Saldo", nueva.Saldo));
+                comm.Parameters.Add(new SqlParameter("@TipoCuenta", nueva.TipoCuenta));
+                comm.Parameters.Add(new SqlParameter("@Estado", nueva.Estado));
+                comm.Parameters.Add(new SqlParameter("@IdCliente", nueva.Cliente.Id));
+
+                id = Convert.ToInt32(comm.ExecuteScalar());
+            }
+            return id;
+        }
+    }
+}

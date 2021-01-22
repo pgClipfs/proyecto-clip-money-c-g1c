47d84c6148e42c956d4f581fadd49aed405b27f9
Ignacio Aguero <ignaciomaguero@gmail.com>
2021-01-22 14:33:43 -0300

    imaplementacion de modulo validar cuenta

---
+++ Models/PersonaActualizarFoto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtualWallet.Models
+{
+    public class PersonaActualizarFoto
+    {
+
+        public PersonaActualizarFoto()
+        {
+        }
+
+        public int Id { get; set; }
+        public string FotoFrente { get; set; }
+        public string FotoReverso { get; set; }
+
+    }
+}
